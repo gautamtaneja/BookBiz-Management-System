0d43f850bcedfd8677a72cd4b0239076b108cc46
Gautam Taneja <gautam.taneja@outlook.com>
2022-03-29 00:12:01 +0530

    add client and add author

---
+++ BookBiz Management System/DataAccess/DB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBiz_Management_System.DataAccess
+{
+    public class DB : DbContext
+    {
+
+    }
+}
