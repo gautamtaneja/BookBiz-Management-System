798a083ca8134890b8fb7c03089d3013f7f751d2
Gautam Taneja <gautam.taneja@outlook.com>
2022-03-29 00:31:45 +0530

    dash

---
+++ BookBiz Management System/MISDashboard.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace BookBiz_Management_System
+{
+    partial class MISDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MISDashboard));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.logout = new System.Windows.Forms.Button();
+            this.user_email = new System.Windows.Forms.Label();
+            this.add_employee = new System.Windows.Forms.Button();
+            this.search_employee = new System.Windows.Forms.Button();
+            this.add_user = new System.Windows.Forms.Button();
+            this.search_user = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Sitka Text", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dashboard";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(699, 44);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(453, 36);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 38);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // logout
+            // 
+            this.logout.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.logout.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logout.Location = new System.Drawing.Point(645, 38);
+            this.logout.Name = "logout";
+            this.logout.Size = new System.Drawing.Size(81, 36);
+            this.logout.TabIndex = 4;
+            this.logout.Text = "Logout";
+            this.logout.UseVisualStyleBackColor = false;
+            // 
+            // user_email
+            // 
+            this.user_email.AutoSize = true;
+            this.user_email.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.user_email.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user_email.Location = new System.Drawing.Point(503, 45);
+            this.user_email.Name = "user_email";
+            this.user_email.Size = new System.Drawing.Size(136, 21);
+            this.user_email.TabIndex = 5;
+            this.user_email.Text = "Demo@gmail.com";
+            // 
+            // add_employee
+            // 
+            this.add_employee.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.add_employee.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add_employee.Location = new System.Drawing.Point(245, 184);
+            this.add_employee.Name = "add_employee";
+            this.add_employee.Size = new System.Drawing.Size(153, 52);
+            this.add_employee.TabIndex = 6;
+            this.add_employee.Text = "Add Employee";
+            this.add_employee.UseVisualStyleBackColor = false;
+            // 
+            // search_employee
+            // 
+            this.search_employee.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.search_employee.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search_employee.Location = new System.Drawing.Point(481, 184);
+            this.search_employee.Name = "search_employee";
+            this.search_employee.Size = new System.Drawing.Size(150, 52);
+            this.search_employee.TabIndex = 7;
+            this.search_employee.Text = "Search Employee";
+            this.search_employee.UseVisualStyleBackColor = false;
+            // 
+            // add_user
+            // 
+            this.add_user.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.add_user.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add_user.Location = new System.Drawing.Point(245, 295);
+            this.add_user.Name = "add_user";
+            this.add_user.Size = new System.Drawing.Size(153, 52);
+            this.add_user.TabIndex = 8;
+            this.add_user.Text = "Add User";
+            this.add_user.UseVisualStyleBackColor = false;
+            // 
+            // search_user
+            // 
+            this.search_user.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.search_user.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search_user.Location = new System.Drawing.Point(481, 295);
+            this.search_user.Name = "search_user";
+            this.search_user.Size = new System.Drawing.Size(150, 52);
+            this.search_user.TabIndex = 9;
+            this.search_user.Text = "Search User";
+            this.search_user.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.logout);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.user_email);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(77, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(771, 377);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dashboard";
+            // 
+            // Deshboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 533);
+            this.Controls.Add(this.search_user);
+            this.Controls.Add(this.add_user);
+            this.Controls.Add(this.search_employee);
+            this.Controls.Add(this.add_employee);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Deshboard";
+            this.Text = "MIS Deshboard";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button logout;
+        private System.Windows.Forms.Label user_email;
+        private System.Windows.Forms.Button add_employee;
+        private System.Windows.Forms.Button search_employee;
+        private System.Windows.Forms.Button add_user;
+        private System.Windows.Forms.Button search_user;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
