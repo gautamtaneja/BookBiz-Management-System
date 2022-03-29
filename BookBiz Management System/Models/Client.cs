891b7ce8f69cb4bab65e80b3d8b12180a7eefb48
Gautam Taneja <gautam.taneja@outlook.com>
2022-03-29 21:41:51 +0530

    db

--- BookBiz Management System/Models/Client.cs
+++ BookBiz Management System/Models/Client.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,7 @@ namespace BookBiz_Management_System.Models
 {
     public class Client
     {
+        [Key, Column(Order = 1)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Street { get; set; }
@@ -16,7 +19,7 @@ namespace BookBiz_Management_System.Models
         public int PhoneNumber { get; set; }
         public int FaxNumber { get; set; }
         public int CreditLimit { get; set; }
-        public Roles Role { get; set; }
+        public Role Role { get; set; }
 
     }
 }
