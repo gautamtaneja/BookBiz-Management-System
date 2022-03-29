da48336c640ef348c8cc631e9c80bd8500c06761
Rohit kaushal <kaushalrohit507@gmail.com>
2022-03-28 17:19:12 -0700

    Login form

--- BookBiz Management System/Login.cs
+++ BookBiz Management System/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BookBiz_Management_System
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
